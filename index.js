3fa958335e152cb9471bbeeaecf75ffa6a2694e4
wkoslik <wkoslik@gmail.com>
2021-01-12 18:50:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+//how to have hidden secret variables
+//how to set up get around these secret variables?
+//environmental variables are global variables that need to be hidden from the front end --> .env files
